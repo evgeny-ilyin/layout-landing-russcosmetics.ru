12fdb9ca2bb61c4f2f25ffb219ba89dfc7978247
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2024-07-28 11:22:47 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import * as fn from "./modules/functions.js";
+// import { useDynamicAdapt } from "./modules/dynamicAdapt.js";
+
+addEventListener("DOMContentLoaded", () => {
+	// useDynamicAdapt();
+
+	fn.isTouchDevice();
+
+	// fn.isWebp();
+	// fn.stickyHeader();
+	// fn.closeMenuHandler();
+});
+
+// import "./modules/cookies.js";
